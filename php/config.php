bdd73d1691ee3008e04540f2394b9ac968015374
Kamran <Kamranislam8@gmail.com>
2026-01-13 19:27:58 +0600

    Update database configuration in config.php to enable live settings and comment out local settings for development.

--- php/config.php
+++ php/config.php
@@ -7,19 +7,19 @@
 
 
 
-// Database configuration local database
-// define('DB_HOST', 'localhost');
-// define('DB_USER', 'telerxb2_telerx');
-// define('DB_PASS', '&+;*LkaHNYztJ+{E');
-// define('DB_NAME', 'telerxb2_telerx_db');
+// Database configuration live database
+define('DB_HOST', 'localhost');
+define('DB_USER', 'telerxb2_telerx');
+define('DB_PASS', '&+;*LkaHNYztJ+{E');
+define('DB_NAME', 'telerxb2_telerx_db');
 
 
 
 // Database configuration local
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '123');
-define('DB_NAME', 'telerx_db');
+// define('DB_HOST', 'localhost');
+// define('DB_USER', 'root');
+// define('DB_PASS', '123');
+// define('DB_NAME', 'telerx_db');
 
 
 // Create database connection
