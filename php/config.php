19e0f842be35987a3514622d78788141f2eac7de
Kamranislam0123 <kamranislam8@gmail.com>
2026-02-14 07:11:45 +0000

    updated

--- php/config.php
+++ php/config.php
@@ -12,17 +12,12 @@ define('DB_NAME', 'telerxb2_telerx_db');
 
 
 // Database configuration local
-<<<<<<< HEAD
 // define('DB_HOST', 'localhost');
 // define('DB_USER', 'root');
 // define('DB_PASS', '123');
 // define('DB_NAME', 'telerx_db');
-=======
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', 'telerx_db');
->>>>>>> aab903b1d666da76887b271bf3477ba23b26b34c
+
+
 
 
 // Create database connection
