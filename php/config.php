3ddef6d7d6179e6c11072c2a0fc29794c3567dbf
Md. Mehedi Hassan <svdopro@gmail.com>
2026-02-27 01:19:15 +0600

    Clean Unused Files
    Please update

--- php/config.php
+++ php/config.php
@@ -10,23 +10,12 @@
 // define('DB_PASS', '&+;*LkaHNYztJ+{E');
 // define('DB_NAME', 'telerxb2_telerx_db');
 
-
-// Database configuration local
-// define('DB_HOST', 'localhost');
-// define('DB_USER', 'root');
-// define('DB_PASS', '');
-// define('DB_NAME', 'telerx_db');
-
 // Database configuration local kamran
 
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
-define('DB_PASS', '123');
+define('DB_PASS', '');
 define('DB_NAME', 'telerx_db');
-
-
-
-
 
 // Create database connection
 function getDBConnection() {
